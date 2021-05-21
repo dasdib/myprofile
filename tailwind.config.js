b923f6920ca21d87ed33e987f87fcc85865ea003
DAS, DIBYENDU [ITSUS non J&J] <ddas446@its.jnj.com>
2021-05-22 04:00:42 +0530

    Update styling on About us

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,7 @@ module.exports = {
       inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
       none: "none",
       "top-white":
-        "0 10px 15px -3px rgba(0, 0, 0, 0.04), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+        "0 10px 15px -3px rgba(0, 0, 0, 0.01), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
     },
   },
   variants: {
